a545e84207034a349d5711aa950c3a10200f42b1
George-hardworking <zkb1141823621@outlook.com>
2026-08-13 21:37:02 +0800

    add: initial implementation of data exploration module with configuration and utility functions

---
+++ src/rpt_re/data/__init__.py
@@ -0,0 +1,17 @@
+from rpt_re.data.explore import (
+    OHLC_DTYPES,
+    csv_row_count,
+    list_data_files,
+    missing_value_stats,
+    read_csv_head,
+    read_csv_sample,
+)
+
+__all__ = [
+    "OHLC_DTYPES",
+    "csv_row_count",
+    "list_data_files",
+    "missing_value_stats",
+    "read_csv_head",
+    "read_csv_sample",
+]
